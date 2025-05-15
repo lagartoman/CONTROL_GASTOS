d23643badf64fae94e1a53bebe7de45a659c2c48
lagartoman <70228453+lagartoman@users.noreply.github.com>
2025-05-15 11:53:59 -0600

    Cambio de colores a las pantallas

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,17 @@ class MiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Control de Gastos',
-      theme: ThemeData(primarySwatch: Colors.teal),
+      title: 'Control de Gastos', 
+      theme: ThemeData(// Color de fondo global
+        scaffoldBackgroundColor: const Color.fromARGB(255, 143, 117, 228), // Fondo claro
+        // Tema de texto global
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Color.fromARGB(221, 199, 248, 4)), // Texto principal
+          bodyMedium: TextStyle(color: Color.fromARGB(157, 101, 250, 2)), // Texto secundario
+        ),
+        // Color primario (botones, AppBar, etc.)
+        primarySwatch: Colors.teal,
+      ),
       home: const HomeScreen(),
     );
   }
